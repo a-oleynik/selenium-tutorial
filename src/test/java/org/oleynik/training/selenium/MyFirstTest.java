2dd4ee9bc347475475c1ee2445f94514fb807f6e
Andrey Oleynik <andrei.oleynik@gmail.com>
2017-10-21 14:59:37 +0200

    Switched to webdriver 3.6.0

--- src/test/java/org/oleynik/training/selenium/MyFirstTest.java
+++ src/test/java/org/oleynik/training/selenium/MyFirstTest.java
@@ -1,11 +1,10 @@
 package org.oleynik.training.selenium;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.HasCapabilities;
+import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleContains;
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class MyFirstTest extends BaseTest{
     @Test
@@ -13,7 +12,8 @@ public class MyFirstTest extends BaseTest{
         driver.navigate().to("http://www.google.com");
         //((HasCapabilities)driver).getCapabilities();
         driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("btnG")).click();
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+        //driver.findElement(By.name("btnK")).click();
         wait.until(titleContains("webdriver"));
     }
 }
